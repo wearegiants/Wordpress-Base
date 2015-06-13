b955c0d9167ff92b2d3757ebd80ec4eb353b3d4b
Craig Fowler <craig@wearegiants.com>
2015-06-13 12:22:13 -0700

    tweaked some widths

--- lib/config.general.php
+++ lib/config.general.php
@@ -1,4 +1,4 @@
 <?php
 
-  $mainContent_width = 'fs-cell fs-lg-7 fs-md-4 fs-sm-3';
-  $mainSidebar_width = 'fs-cell fs-lg-4 fs-md-2 fs-sm-3 fs-right';
+  $mainContent_width = 'fs-cell fs-xl-7 fs-lg-8 fs-md-6 fs-sm-3';
+  $mainSidebar_width = 'fs-cell fs-xl-4 fs-lg-4 fs-md-6 fs-sm-3 fs-right';

304715963667c444bbc1ed801dad12b56045f625
Craig Fowler <craig@wearegiants.com>
2015-08-21 11:35:21 -0700

    updates

--- page.php
+++ page.php
@@ -1,10 +1,22 @@
 <?php Themewrangler::setup_page();get_header(); ?>
 
-<div ng-controller="mycontroller">
-	<article ng-repeat="post in posts">
-		<h3>{{ post.title }}</h3>
-		<p>{{ post.excerpt }}</p>
-	</article>
+<div class="fs-row">
+<section id="content" role="main" class="<?php echo $mainContent_width; ?>">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="header">
+<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+</header>
+<section class="entry-content">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+<?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
+</section>
+</article>
+<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+<?php endwhile; endif; ?>
+</section>
+<?php get_sidebar(); ?>
 </div>
 
 <?php get_footer(); ?>

4a3315767b076c8edfa210a294427a9f8061c855
Craig Fowler <craig@wearegiants.com>
2016-08-19 11:03:37 -0700

    added homepage

--- front-page.php
+++ front-page.php
@@ -7,21 +7,32 @@
   $images = get_field('gallery');
 
 ?>
-
-<div class="home-page">
-  <div class="fs-row">
-    <div class="fs-cell fs-all-full">
-      <?php the_content(); ?>
+<div class="hero hero--wh relative">
+  <div class="home-mission pinned pinned--top">
+    <div class="fs-row">
+      <div class="fs-cell fs-all-full color--white">
+        <hr class="invisible">
+        <?php the_content(); ?>
+      </div>
     </div>
   </div>
-</div>
-
-<div class="carousel" data-carousel-options='{"theme":"fs-light"}'>
-<?php foreach($images as $image): ?>
-  <div class="home-carousel__slide carousel__slide">
-    <div class="hero hero--wh wallpaper" data-background-options='{"source":"<?php echo $image["url"]; ?>"}'></div>
+  <div class="carousel home-carousel" data-carousel-options='{"single": true, "lazy":true, "theme":"fs-light"}'>
+  <?php foreach($images as $image): ?>
+    <div class="home-carousel__slide carousel__slide">
+      <?php if($image['caption']): ?>
+      <div class="home-carousel__slide-caption pinned pinned--bottom">
+        <div class="fs-row">
+          <div class="fs-cell fs-all-full">
+            <?php echo $image['caption']; ?>
+            <hr class="invisible compact">
+          </div>
+        </div>
+      </div>
+      <?php endif; ?>
+      <div class="hero hero--wh wallpaper" data-background-options='{"source":"<?php echo $image["url"]; ?>"}'></div>
+    </div>
+  <?php endforeach; ?>
   </div>
-<?php endforeach; ?>
 </div>
 
 

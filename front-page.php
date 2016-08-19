8cefbc4993481a2322fe2685b15150d327f5d35c
Craig Fowler <craig@wearegiants.com>
2016-08-19 11:26:39 -0700

    home updates

--- front-page.php
+++ front-page.php
@@ -7,34 +7,83 @@
   $images = get_field('gallery');
 
 ?>
-<div class="hero hero--wh relative">
-  <div class="home-mission pinned pinned--top">
+
+<div class="home-mission pinned pinned--top hero hero--lg">
+  <div class="centered">
     <div class="fs-row">
-      <div class="fs-cell fs-all-full color--white">
-        <hr class="invisible">
+      <div class="fs-cell fs-all-full color--white text-center">
         <?php the_content(); ?>
       </div>
     </div>
   </div>
-  <div class="carousel home-carousel" data-carousel-options='{"single": true, "lazy":true, "theme":"fs-light"}'>
-  <?php foreach($images as $image): ?>
-    <div class="home-carousel__slide carousel__slide">
-      <?php if($image['caption']): ?>
-      <div class="home-carousel__slide-caption pinned pinned--bottom">
-        <div class="fs-row">
-          <div class="fs-cell fs-all-full">
-            <?php echo $image['caption']; ?>
-            <hr class="invisible compact">
+</div>
+<div class="carousel home-carousel" data-carousel-options='{"controls": false, "single": true, "lazy":true, "theme":"fs-light"}'>
+<?php foreach($images as $image): ?>
+  <div class="home-carousel__slide carousel__slide">
+    <?php if($image['caption']): ?>
+    <div class="home-carousel__slide-caption pinned pinned--bottom">
+      <div class="fs-row">
+        <div class="fs-cell fs-all-full text-center">
+          <?php echo $image['caption']; ?>
+          <hr class="invisible compact">
+        </div>
+      </div>
+    </div>
+    <?php endif; ?>
+    <div class="hero hero--lg wallpaper" data-background-options='{"source":"<?php echo $image["url"]; ?>"}'></div>
+  </div>
+<?php endforeach; ?>
+</div>
+
+<hr class="invisible">
+
+<div class="home-sections">
+  <div class="fs-row">
+    <div class="fs-cell fs-lg-11 fs-md-5 fs-sm-3 fs-centered">
+      <div class="fs-row">
+        <div class="fs-cell fs-lg-half fs-md-half fs-sm-3">
+          <div class="hero relative">
+            <div class="hero--sm bg--gray pinned pinned--bottom"></div>
+          </div>
+        </div>
+        <div class="fs-cell fs-lg-half fs-md-half fs-sm-3">
+          <div class="hero relative">
+            <div class="hero bg--gray pinned pinned--bottom"></div>
           </div>
         </div>
       </div>
-      <?php endif; ?>
-      <div class="hero hero--wh wallpaper" data-background-options='{"source":"<?php echo $image["url"]; ?>"}'></div>
     </div>
-  <?php endforeach; ?>
+  </div>
+  <hr class="invisible">
+  <div class="fs-row">
+    <div class="fs-cell fs-lg-10 fs-md-5 fs-sm-3 fs-centered">
+      <div class="fs-row">
+        <div class="fs-cell fs-lg-half fs-md-half fs-sm-3">
+          <div class="hero relative">
+            <div class="hero bg--gray pinned pinned--bottom"></div>
+          </div>
+        </div>
+        <div class="fs-cell fs-lg-half fs-md-half fs-sm-3">
+          <div class="hero relative">
+            <div class="hero--sm bg--gray pinned pinned--top"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr class="invisible">
+  <div class="fs-row">
+    <div class="fs-cell fs-lg-11 fs-md-5 fs-sm-3 fs-centered">
+      <div class="fs-row">
+        <div class="fs-cell fs-all-full">
+          <div class="hero relative">
+            <div class="hero bg--gray pinned pinned--bottom"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
-
 
 
 <?php get_footer(); ?>
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -2,11 +2,13 @@
 // @codekit-prepend "../bower_components/formstone/dist/js/core.js"
 // @codekit-prepend "../bower_components/formstone/dist/js/touch.js"
 // @codekit-prepend "../bower_components/formstone/dist/js/transition.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/analytics.js"
 // @codekit-prepend "../bower_components/formstone/dist/js/mediaquery.js"
+
 // @codekit-prepend "../bower_components/formstone/dist/js/swap.js"
-// @codekit-prepend "../bower_components/formstone/dist/js/checkbox.js"
 // @codekit-prepend "../bower_components/formstone/dist/js/carousel.js"
 // @codekit-prepend "../bower_components/formstone/dist/js/background.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/asap.js"
 
 // Other Plugins
 // @codekit-prepend "../bower_components/components-modernizr/modernizr.js"
